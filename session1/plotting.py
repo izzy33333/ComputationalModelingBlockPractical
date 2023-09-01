e6ae192958e1b97813b171cfc708c5398c3ac861
JanGrohn <jangrohn@gmail.com>
2023-09-01 14:51:31 +0100

    test

--- session1/plotting.py
+++ session1/plotting.py
@@ -69,3 +69,82 @@ def plot_schedule(opt1Rewarded, trueProbability, probOpt1 = None):
   fig.update_yaxes(range=[-0.05, 1.05])
 
   return fig
+
+def plot_interactive_RL_model(opt1Rewarded, trueProbability, probOpt1, RL_model):
+  '''
+  Plots the experimental schedule and the RL model estimate with sliders to
+  change the model parameters.
+
+    Parameters:
+        opt1rewarded(int array): 1 if option 1 is rewarded on a trial, 0 if
+          option 2 is rewarded on a trial.
+        trueProbability(float array): The probability with which option 1 is
+          rewareded on each trial.
+        probOpt1(float array): how likely option 1 is rewarded on each trial
+          according to the RL model. 
+        RL_model(function): the RL model function to use to generate probOpt1.
+  '''
+
+  # this initiates some sliders that can be used to dynamically change aprameter values
+  alphaSlider        = widgets.FloatSlider(
+                                  value=0.1,
+                                  max=1,
+                                  min=0.001,
+                                  step=0.01,
+                                  description='alpha:',
+                                  continuous_update=False
+                                  )
+
+  startingProbSlider = widgets.FloatSlider(
+                                  value=0.5,
+                                  max=1,
+                                  min=0,
+                                  step =0.01,
+                                  description='startingProb:',
+                                  continuous_update=False
+                                  )
+  trueProbSlider     = widgets.FloatSlider(
+                                  value=0.8,
+                                  max=1,
+                                  description='trueProb:',
+                                  continuous_update=False
+                                  )
+
+  sliders = widgets.VBox(children=[alphaSlider,
+                                  startingProbSlider,
+                                  trueProbSlider])
+
+  # call the figure function we wrote and make it interactive
+  fig = go.FigureWidget(plot_schedule(opt1Rewarded,trueProbability,probOpt1))
+
+  # function to run if alpha or startingProb have changed
+  def change_model(change):
+    # get current values
+    opt1Rewarded = fig.data[0].y
+    trueProbability = trueProbSlider.value
+    # rerun the RL model
+    probOpt1 = RL_model(opt1Rewarded,alphaSlider.value,startingProbSlider.value)
+    # update the figure
+    with fig.batch_update():
+      fig.data[2].y = probOpt1
+
+  # function to run if trueProbability has changed
+  def change_schedule(change):
+    # generate a new schedule
+    trueProbability = np.ones(nTrials, dtype = float)*trueProbSlider.value
+    opt1Rewarded = generate_schedule(trueProbability)
+    # rerun the RL model
+    probOpt1 = RL_model(opt1Rewarded,alphaSlider.value,startingProbSlider.value)
+    # update the figure
+    with fig.batch_update():
+      fig.data[0].y = opt1Rewarded
+      fig.data[1].y = trueProbability
+      fig.data[2].y = probOpt1
+
+  # run the functions if a slider value changes
+  alphaSlider.observe(change_model, names="value")
+  startingProbSlider.observe(change_model, names="value")
+  trueProbSlider.observe(change_schedule, names="value")
+
+  # show the figure and the sliders
+  display(widgets.VBox([sliders, fig]))
