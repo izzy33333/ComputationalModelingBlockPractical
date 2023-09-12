cc7047ec7db25d71a1fa65542e5f3d24e46a9fde
JanGrohn <jangrohn@gmail.com>
2023-09-12 12:07:50 +0100

    Create loading.py

---
+++ BlockPracticalCode/session3/loading.py
@@ -0,0 +1,38 @@
+# numpy is a libarary used to do all kinds of mathematical operations
+import numpy as np
+
+# load in the dataset
+from scipy.io import loadmat
+blain_data = loadmat("Blain_MoodTracksLearning_data.mat")
+
+def load_blain(ID, data = blain_data):
+
+  if ID < 37:
+    order = "stable2volatile_data"
+  else:
+    order = "volatile2stable_data"
+    ID -= 37
+
+  trueProbability = data[order]["main"][0][ID]["task_mu"][0][0][0]
+  outcome = data[order]["main"][0][ID]["maintask"][0][0][:,14] > 0
+  numTrials = len(outcome)
+  choice1       = np.empty(numTrials, dtype = int)
+  magOpt1       = np.empty(numTrials, dtype = int)
+  magOpt2       = np.empty(numTrials, dtype = int)
+  opt1Rewarded  = np.empty(numTrials, dtype = int)
+  for i in range(numTrials):
+    choice1[i]  = 2 - data[order]["main"][0][ID]["list_pair"][0][0][int(data[order]["main"][0][ID]["maintask"][0][0][i,4]-1),i]
+    magOpt1[i]  = data[order]["main"][0][ID]["mat_mag"][0][0][data[order]["main"][0][ID]["index_mag"][0][0][:,i][0]-1,0]
+    magOpt2[i]  = data[order]["main"][0][ID]["mat_mag"][0][0][data[order]["main"][0][ID]["index_mag"][0][0][:,i][0]-1,1]
+    if choice1[i]:
+      if outcome[i]:
+        opt1Rewarded[i] = 1
+      else:
+        opt1Rewarded[i] = 0
+    else:
+      if outcome[i]:
+        opt1Rewarded[i] = 0
+      else:
+        opt1Rewarded[i] = 1
+
+  return trueProbability, choice1, magOpt1, magOpt2, opt1Rewarded
